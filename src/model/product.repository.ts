2c6a681f6575d7808193f2dff955b2afadecabee
diego <diego.cortinagarcia@gmail.com>
2022-06-01 10:42:45 +0200

    added info to constructor en prod repo

--- src/model/product.repository.ts
+++ src/model/product.repository.ts
@@ -6,5 +6,12 @@ export class ProductRepository {
   private products: Product[] = [];
   private categories: string[] = [];
 
-  constructor(private dataSource: StaticDataSource) { }
+  constructor(private dataSource: StaticDataSource) {
+    dataSource.getProducts().subscribe(
+      data => {
+        this.products = data;
+        this.categories = data.map(p => p.category).filter((c, index, array) => array.indexOf(c) == index).sort();
+      }
+    );
+  }
 }

a081293c32260d4b611d3d47fffca37f5353e04d
diego <diego.cortinagarcia@gmail.com>
2022-06-01 13:45:38 +0200

    data

--- src/model/static.datasource.ts
+++ src/model/static.datasource.ts
@@ -17,7 +17,7 @@ export class StaticDataSource {
     new Product(9, "Product 9", 240, "Cat 1", "Product 9 (Cat 1)"),
     new Product(10, "Product 10", 50, "Cat 2", "Product 10 (Cat 2)"),
     new Product(11, "Product 11", 80, "Cat 3", "Product 11 (Cat 3)"),
-    new Product(12, "Product 12", 13, "Cat 1", "Product 12 Cat 1)"),
+    new Product(12, "Product 12", 13, "Cat 1", "Product 12 (Cat 1)"),
     new Product(13, "Product 13", 6530, "Cat 2", "Product 13 (Cat 2)"),
     new Product(14, "Product 14", 9765, "Cat 1", "Product 14 (Cat 1)"),
     new Product(15, "Product 15", 5688, "Cat 3", "Product 15 (Cat 3)"),

fc4b99ab75b4f1b0307d1376e7b1fe2888c73e07
diego <diego.cortinagarcia@gmail.com>
2022-06-02 10:35:22 +0200

    linked injectable

--- src/model/model.module.ts
+++ src/model/model.module.ts
@@ -1,9 +1,10 @@
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 import { NgModule } from '@angular/core';
+import { Cart } from './cart.model';
 
 @NgModule({
-  providers: [StaticDataSource, ProductRepository]
+  providers: [StaticDataSource, ProductRepository, Cart]
 })
 
 export class ModelModule { }

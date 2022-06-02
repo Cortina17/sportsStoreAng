1d4495c8292c1ebc11b8fbd1cef6d63c476d563f
diego <diego.cortinagarcia@gmail.com>
2022-06-02 10:26:42 +0200

    cart logic update price

--- src/model/cart.model.ts
+++ src/model/cart.model.ts
@@ -22,6 +22,14 @@ export class Cart {
     this.recalculate();
   }
 
+  updateQuantity(product: Product, quantity: number) {
+    let line = this.lines.find(line => line.product.id == product.id);
+    if (line != undefined) {
+      line.quantity = Number(quantity);
+    }
+    this.recalculate();
+  }
+
   private recalculate() {
     this.itemCount = 0;
     this.cartPrice = 0;

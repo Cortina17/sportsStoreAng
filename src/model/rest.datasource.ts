7fa357ea12bd717b2c976ccaa71fb2e2ffd3360a
diego <diego.cortinagarcia@gmail.com>
2022-06-03 13:36:59 +0200

    auth service

--- src/model/rest.datasource.ts
+++ src/model/rest.datasource.ts
@@ -1,6 +1,6 @@
 import { Order } from './order.model';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http"
 
@@ -10,9 +10,21 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
   baseUrl: string = "";
+  auth_token: string;
+
   constructor(private http: HttpClient) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
   }
+
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http.post<any>(this.baseUrl + "login", {
+      name: user, password: pass
+    }).pipe(map(response => {
+      this.auth_token = response.success ? response.token : null;
+      return response.success;
+    }));
+  }
+
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl + "products");

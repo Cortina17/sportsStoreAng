b39bcdbff1a9279bd230d631be5095cef9565205
diego <diego.cortinagarcia@gmail.com>
2022-06-06 09:04:10 +0200

    delete order

--- src/model/order.repository.ts
+++ src/model/order.repository.ts
@@ -27,6 +27,10 @@ export class OrderRepository {
     this.dataSource.updateOrder(order).subscribe((order) => this.orders.splice(this.orders.findIndex((o) => o.id == order.id), 1, order));
   }
 
+  deleteOrder(id: number) {
+    this.dataSource.deleteOrder(id).subscribe((order) => this.orders.splice(this.orders.findIndex((o) => id == o.id), 1));
+  }
+
   loadOrders() {
     this.loaded = true;
     this.dataSource.getOrders().subscribe((orders) => (this.orders = orders));

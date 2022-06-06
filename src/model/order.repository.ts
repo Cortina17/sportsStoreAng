8811278b533e7c92fefdeadb0a2ef2ebee6981dd
diego <diego.cortinagarcia@gmail.com>
2022-06-06 09:00:58 +0200

    update order

--- src/model/order.repository.ts
+++ src/model/order.repository.ts
@@ -7,16 +7,28 @@ import { Observable } from 'rxjs';
 export class OrderRepository {
 
   private orders: Order[] = [];
+  private loaded: boolean = false;
 
   //constructor(private dataSource: StaticDataSource) { }
   constructor(private dataSource: RestDataSource) { }
 
-  getOrders() {
+  getOrders(): Order[] {
+    if (!this.loaded) {
+      this.loadOrders();
+    }
     return this.orders;
   }
 
   saveOrder(order: Order): Observable<Order> {
     return this.dataSource.saveOrder(order);
+  }
 
+  updateOrder(order: Order) {
+    this.dataSource.updateOrder(order).subscribe((order) => this.orders.splice(this.orders.findIndex((o) => o.id == order.id), 1, order));
+  }
+
+  loadOrders() {
+    this.loaded = true;
+    this.dataSource.getOrders().subscribe((orders) => (this.orders = orders));
   }
 }
